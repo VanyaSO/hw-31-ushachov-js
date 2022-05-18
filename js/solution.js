2c6542468913830ff87c0e5eea3c86a7d8d55435
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-05-18 16:46:32 +0300

    ✅

---
+++ js/solution.js
@@ -0,0 +1,12 @@
+let bodyChilds = document.body.childNodes;
+
+const arr = [];
+
+for(let li of bodyChilds){
+    arr.push(document.querySelector('ul > li').innerHTML);
+}
+
+console.log(arr);
+// for(){
+//
+// }
